36ff1f10183c3d48e618d0ecef76526988799ef1
ddloveyy <dyanonymous@163.com>
2017-01-03 16:02:14 +0800

    thrid commit

---
+++ app/src/main/java/com/ywl5320/pickaddress/UniqueCodeActivity.java
@@ -0,0 +1,12 @@
+package com.ywl5320.pickaddress;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class UniqueCodeActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
